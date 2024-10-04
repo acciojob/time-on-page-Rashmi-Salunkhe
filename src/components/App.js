75a507fc68864738faab6cfa71e8adc3843375e0
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-10-04 09:57:29 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
-
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds((prevSeconds) => prevSeconds + 1);
+    }, 1000);
+
+    // Clean up the interval when the component unmounts
+    return () => {
+      clearInterval(interval);
+    };
+  }, [seconds]);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      {/* <p>You\'ve been on this page for 0 seconds.</p> */}
+      <p>You've been on this page for {seconds} seconds.</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
